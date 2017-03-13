f1ae456c4a1bcb0e32f94648b21a77e026ee2d5e
Anthony Broome <anthonybroome87@gmail.com>
2017-03-13 11:52:12 +0000

    Moving outputs to maps

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,41 @@
-output "admin_ssh_private" {
-  value = "${tls_private_key.admin_ssh.private_key_pem}"
+output "admin_data" {
+  value = {
+    location = "${azurerm_resource_group.admin.location}"
+    resource_group_name = "${azurerm_resource_group.admin.name}"
+    storage_account_name = "${azurerm_storage_account.admin.name}"
+    storage_account_endpoint = "${azurerm_storage_account.admin.primary_blob_endpoint}"
+    storage_container_name = "${azurerm_storage_container.admin.name}"
+    subnet_name = "${azurerm_subnet.admin_subnet.name}"
+    subnet_id = "${azurerm_subnet.admin_subnet.id}"
+    ssh_private = "${tls_private_key.admin_ssh.private_key_pem}"
+    ssh_public = "${tls_private_key.admin_ssh.public_key_pem}"
+  }
 }
 
-output "admin_ssh_public" {
-  value = "${tls_private_key.admin_ssh.public_key_pem}"
+output "dev_data" {
+  value = {
+    location = "${azurerm_resource_group.dev.location}"
+    resource_group_name = "${azurerm_resource_group.dev.name}"
+    storage_account_name = "${azurerm_storage_account.dev.name}"
+    storage_account_endpoint = "${azurerm_storage_account.dev.primary_blob_endpoint}"
+    storage_container_name = "${azurerm_storage_container.dev.name}"
+    subnet_name = "${azurerm_subnet.dev_subnet.name}"
+    subnet_id = "${azurerm_subnet.dev_subnet.id}"
+    ssh_private = "${tls_private_key.dev_ssh.private_key_pem}"
+    ssh_public = "${tls_private_key.dev_ssh.public_key_pem}"
+  }
 }
 
-output "dev_ssh_private" {
-  value = "${tls_private_key.dev_ssh.private_key_pem}"
-}
-
-output "dev_ssh_public" {
-  value = "${tls_private_key.dev_ssh.public_key_pem}"
-}
-
-output "prod_ssh_private" {
-  value = "${tls_private_key.prod_ssh.private_key_pem}"
-}
-
-output "prod_ssh_public" {
-  value = "${tls_private_key.prod_ssh.public_key_pem}"
+output "prod_data" {
+  value = {
+    location = "${azurerm_resource_group.prod.location}"
+    resource_group_name = "${azurerm_resource_group.prod.name}"
+    storage_account_name = "${azurerm_storage_account.prod.name}"
+    storage_account_endpoint = "${azurerm_storage_account.prod.primary_blob_endpoint}"
+    storage_container_name = "${azurerm_storage_container.prod.name}"
+    subnet_name = "${azurerm_subnet.prod_subnet.name}"
+    subnet_id = "${azurerm_subnet.prod_subnet.id}"
+    ssh_private = "${tls_private_key.prod_ssh.private_key_pem}"
+    ssh_public = "${tls_private_key.prod_ssh.public_key_pem}"
+  }
 }
