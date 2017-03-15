cc056da6480a1011e38aed80721018fd2ac4321c
Anthony Broome <anthonybroome87@gmail.com>
2017-03-15 14:27:11 +0000

    ssh cleanup

--- ssh.tf
+++ ssh.tf
@@ -4,58 +4,51 @@ resource "tls_private_key" "admin_ssh" {
   algorithm = "RSA"
 }
 
-# Export admin ssh key to file
-resource "null_resource" "admin_private" {
-  depends_on = ["tls_private_key.admin_ssh"]
-
-  provisioner "local-exec" {
-    command = "echo '${tls_private_key.admin_ssh.private_key_pem}' > ${path.cwd}${var.allspark_ssh_dir}/allspark_admin_rsa && chmod 600 ${path.cwd}${var.allspark_ssh_dir}/allspark_admin_rsa"
-  }
+resource "tls_private_key" "prod_ssh" {
+  algorithm = "RSA"
 }
-
-resource "null_resource" "admin_public" {
-  depends_on = ["tls_private_key.admin_ssh"]
-
-  provisioner "local-exec" {
-    command = "echo '${tls_private_key.admin_ssh.public_key_pem}' > ${path.cwd}${var.allspark_ssh_dir}/allspark_admin_rsa.pub && chmod 600 ${path.cwd}${var.allspark_ssh_dir}/allspark_admin_rsa.pub"
-  }
-}
-
 
 resource "tls_private_key" "dev_ssh" {
   algorithm = "RSA"
 }
 
-resource "null_resource" "dev_private" {
-  depends_on = ["tls_private_key.dev_ssh"]
+# Export admin ssh key to file
+resource "null_resource" "ssh_key_gen_dir" {
+  provisioner "local-exec" {
+    command = "mkdir -p ${path.cwd}${var.allspark_ssh_dir}"
+  }
+}
+
+resource "null_resource" "ssh_key_gen" {
+  depends_on = ["null_resource.ssh_key_gen_dir"]
+
+  provisioner "local-exec" {
+    command = "mkdir -p ${path.cwd}${var.allspark_ssh_dir}"
+  }
+
+  provisioner "local-exec" {
+    command = "echo '${tls_private_key.admin_ssh.private_key_pem}' > ${path.cwd}${var.allspark_ssh_dir}/allspark_admin_rsa && chmod 600 ${path.cwd}${var.allspark_ssh_dir}/allspark_admin_rsa"
+  }
+
+  provisioner "local-exec" {
+    command = "echo '${tls_private_key.admin_ssh.public_key_pem}' > ${path.cwd}${var.allspark_ssh_dir}/allspark_admin_rsa.pub && chmod 600 ${path.cwd}${var.allspark_ssh_dir}/allspark_admin_rsa.pub"
+  }
+
+  provisioner "local-exec" {
+    command = "chmod 600 ${path.cwd}${var.allspark_ssh_dir}/allspark_admin_rsa.pub"
+  }
 
   provisioner "local-exec" {
     command = "echo '${tls_private_key.dev_ssh.private_key_pem}' > ${path.cwd}${var.allspark_ssh_dir}/allspark_dev_rsa && chmod 600 ${path.cwd}${var.allspark_ssh_dir}/allspark_dev_rsa"
   }
-}
-
-resource "null_resource" "dev_public" {
-  depends_on = ["tls_private_key.dev_ssh"]
 
   provisioner "local-exec" {
     command = "echo '${tls_private_key.dev_ssh.public_key_pem}' > ${path.cwd}${var.allspark_ssh_dir}/allspark_dev_rsa.pub && chmod 600 ${path.cwd}${var.allspark_ssh_dir}/allspark_dev_rsa.pub"
   }
-}
-
-resource "tls_private_key" "prod_ssh" {
-  algorithm = "RSA"
-}
-
-resource "null_resource" "prod_private" {
-  depends_on = ["tls_private_key.prod_ssh"]
 
   provisioner "local-exec" {
     command = "echo '${tls_private_key.prod_ssh.private_key_pem}' > ${path.cwd}${var.allspark_ssh_dir}/allspark_prod_rsa && chmod 600 ${path.cwd}${var.allspark_ssh_dir}/allspark_prod_rsa"
   }
-}
-
-resource "null_resource" "prod_public" {
-  depends_on = ["tls_private_key.prod_ssh"]
 
   provisioner "local-exec" {
     command = "echo '${tls_private_key.prod_ssh.public_key_pem}' > ${path.cwd}${var.allspark_ssh_dir}/allspark_prod_rsa.pub && chmod 600 ${path.cwd}${var.allspark_ssh_dir}/allspark_prod_rsa.pub"
