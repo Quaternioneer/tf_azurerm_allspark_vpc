010d1666acf91b324b6424575b936717dbcbc36d
Anthony Broome <anthonybroome87@gmail.com>
2017-03-20 09:28:51 +0000

    remove mixed output, bug in terraform

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,14 @@
 output "allspark_data" {
+
   value = {
-    location = "${var.azurerm_location}"
-    resource_group_name = "${azurerm_resource_group.allspark.name}"
-    storage_account_name = "${azurerm_storage_account.allspark.name}"
-    storage_account_endpoint = "${azurerm_storage_account.allspark.primary_blob_endpoint}"
-    ssh_public = "${tls_private_key.ssh.public_key_openssh}"
+
+    common = {
+      location = "${var.azurerm_location}"
+      resource_group_name = "${azurerm_resource_group.allspark.name}"
+      storage_account_name = "${azurerm_storage_account.allspark.name}"
+      storage_account_endpoint = "${azurerm_storage_account.allspark.primary_blob_endpoint}"
+      ssh_public = "${tls_private_key.ssh.public_key_openssh}"
+    }
 
     admin = {
         storage_container_name = "${azurerm_storage_container.admin.name}"
