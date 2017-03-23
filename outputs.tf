907a414f7bc36715e7616ac83f64b848f9277e59
Anthony Broome <anthonybroome87@gmail.com>
2017-03-23 14:55:49 +0000

    NSG per subnet

--- outputs.tf
+++ outputs.tf
@@ -5,15 +5,15 @@ output "allspark_data" {
     ssh_public = "${tls_private_key.ssh.public_key_openssh}"
 
     # Network Settings
-    subnet_name = "${list(azurerm_subnet.subnet.*.name)}"
-    subnet_id = "${list(azurerm_subnet.subnet.*.id)}"
+    subnet_name = "${join("," azurerm_subnet.subnet.*.name)}"
+    subnet_id = "${join(",", azurerm_subnet.subnet.*.id)}"
 
     # Storage Details
-    storage_container_name = "${list(azurerm_storage_container.storage.*.name)}"
+    storage_container_name = "${join(",",azurerm_storage_container.storage.*.name)}"
     storage_account_name = "${azurerm_storage_account.storage.name}"
     storage_account_endpoint = "${azurerm_storage_account.storage.primary_blob_endpoint}"
 
     # Firewall
-    nsg_id = "${list(azurerm_network_security_group.nsg.*.id)}"
+    nsg_id = "${join(",", azurerm_network_security_group.nsg.*.id)}"
   }
 }
