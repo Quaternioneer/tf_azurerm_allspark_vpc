f41ae9e7219f7e7b1adc2ac8f8bf1194ad9d0d16
Anthony Broome <anthonybroome87@gmail.com>
2017-03-24 11:03:26 +0000

    Link NSG to subnets

--- network.tf
+++ network.tf
@@ -17,4 +17,5 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = "${azurerm_resource_group.resource_group.name}"
   virtual_network_name = "${azurerm_virtual_network.vnet.name}"
   address_prefix       = "${element(var.subnets, count.index)}/24"
+  network_security_group_id = "${azurerm_network_security_group.nsg.id}"
 }
