bc210888d392ba6abd5486327a0e3626e884f2da
Anthony Broome <anthonybroome87@gmail.com>
2017-04-04 11:51:40 +0100

    typo in ip variable

--- bastion.tf
+++ bastion.tf
@@ -72,7 +72,7 @@ resource "azurerm_network_security_rule" "bastion" {
     source_port_range           = "*"
     destination_port_range      = "22"
     source_address_prefix       = "Internet"
-    destination_address_prefix  = "${azurerm_network_interface.public_nic.public_ip_address}"
+    destination_address_prefix  = "${azurerm_network_interface.public_nic.ip_address}"
     resource_group_name         = "${azurerm_resource_group.resource_group.name}"
     network_security_group_name = "${azurerm_network_security_group.nsg.name}"
 }
--- outputs.tf
+++ outputs.tf
@@ -17,6 +17,6 @@ output "allspark_data" {
     # Firewall Group - Configure rules externally
     nsg_name = "${azurerm_network_security_group.nsg.name}"
 
-    bastion_ip = "${azurerm_network_interface.public_nic.public_ip_address}"
+    bastion_ip = "${azurerm_network_interface.public_nic.ip_address}"
   }
 }
