e125c729eb98a03fd9b4f865e63aa06ac854f4cd
Nathaniel Davila <nat.admin@inventati.org>
2022-02-25 13:31:17 -0800

    quite a few changes

--- main.tf
+++ main.tf
@@ -14,3 +14,12 @@ resource "azurerm_resource_group" "resource_group" {
     environment = "${var.environment}"
   }
 }
+
+module "allspark" {
+  source  = "github.com/broomyocymru/tf_azurerm_allspark_sql_always_on"
+  allspark = "${module.allspark.allspark_data}"
+  server_name = "allspark"
+  admin_username = "adminusersql"
+  admin_password = "reallySecurePassword909!"
+  db_names = ["app1", "app2", "app3"]
+}
